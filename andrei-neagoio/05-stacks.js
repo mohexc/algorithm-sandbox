b9ca7847ac87dcc70fa6c66a4b68c511b9e4293a
nanut <galunyu@gmail.com>
2021-06-19 17:27:16 +0700

    added stack structre

--- andrei-neagoio/05-stacks.js
+++ andrei-neagoio/05-stacks.js
@@ -1,3 +1,61 @@
+const print = console.log
 
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+    this.bottom = null
+  }
+}
 
+class Stack {
+  constructor() {
+    this.top = null
+    this.bottom = null
+    this.length = 0
+  }
 
+  peek() {
+    return this.top
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.top = newNode
+      this.bottom = newNode
+    }
+    const holdingPointer = this.top
+    this.top = newNode
+    this.top.next = holdingPointer
+    this.length++
+    return this
+  }
+
+  pop() {
+    if (!this.top) {
+      return null
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null
+    }
+    const holdingPointer = this.top
+    this.top = this.top.next
+    this.length--
+    return holdingPointer
+  }
+}
+
+const myStack = new Stack()
+myStack.push('google')
+print(myStack)
+myStack.push('udemy')
+print(myStack)
+myStack.push('discord')
+print(myStack)
+myStack.peek()
+print(myStack)
+myStack.pop()
+print(myStack)
+myStack.pop()
+print(myStack)
