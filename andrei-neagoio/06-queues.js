1464856d699ab2815878832742388bfa4a23ea21
nanut <galunyu@gmail.com>
2021-06-19 17:57:04 +0700

    added queues

---
+++ andrei-neagoio/06-queues.js
@@ -0,0 +1,56 @@
+const print = console.log
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.length = 0
+  }
+
+  peek() {
+    return this.first
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.first = newNode
+      this.last = newNode
+    }
+    else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    this.length++
+    return this
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null
+    }
+    if (this.first === this.last) {
+      this.last = null
+    }
+    const holdingPointer = this.first
+    this.first = this.first.next
+    this.length--
+    return holdingPointer
+  }
+}
+
+const myQueue = new Queue()
+
+print(myQueue.enqueue('Joy'))
+print(myQueue.enqueue('Matt'))
+print(myQueue.enqueue('Pavel'))
+print(myQueue.enqueue('Samir'))
+print(myQueue.enqueue('nut'))
+print(myQueue.peek())
+print(myQueue.dequeue())
