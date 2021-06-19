5f450c51c798108abe69b54a7200636292c9f309
nanut <galunyu@gmail.com>
2021-06-20 02:43:24 +0700

    added trees-stucture

---
+++ andrei-neagoio/07-trees-structure.js
@@ -0,0 +1,154 @@
+const print = console.log
+
+// root     [1]
+// parent   [1][3]
+// child    [2][3][4]  [6][7]
+// leaf     [2][4][6][7]
+// sibling  [2][3][4] [6][7]
+
+
+class Node {
+  constructor(value) {
+    this.left = null
+    this.right = null
+    this.value = value
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+
+  insert(value) {
+    const newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // LEFT
+          if (!currentNode.left) {
+            currentNode.left = newNode
+            return this
+          }
+          currentNode = currentNode.left
+        } else {
+          // RIGHT
+          if (!currentNode.right) {
+            currentNode.right = newNode
+            return this
+          }
+          currentNode = currentNode.right
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    if (!this.root) {
+      return false
+    }
+    let currentNode = this.root
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left
+      }
+      else if (value > currrentNode.left) {
+        currentNode = currentNode.right
+      }
+      else if (currentNode.value === value) {
+        return currentNode
+      }
+    }
+
+    return null
+  }
+
+  remove(value) {
+    if (!this.root) {
+      return false
+    }
+    let currentNode = this.root
+    let parentNode = null
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.right
+      } else if (currentNode.value === value) {
+        // we have a match, get to work!
+        // Option 1 : no righth child:
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left
+            }
+            else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left
+            }
+            else if (currentNode.right.left === nul) {
+              currentNode.right.left = currentNode.left
+              if (parentNode === null) {
+                this.root = currentNode.right
+              } else {
+                if (currentNode.value > parentNode.value) {
+                  parentNode.right = currentNode.right
+                } else {
+                  let leftmost = currentNode.right.left
+                  let leftmostParent = currentNode.right
+                  while (leftmost.left !== null) {
+                    leftmostParent = leftmost
+                    leftmost = leftmost.left
+                  }
+                  leftmostParent.left = leftmost.right
+                  leftmost.left = currentNode.left
+                  leftmost.right = currentNode.right
+                  if (parentNode === null) {
+                    this.root = leftmost
+                  } else {
+                    if (currentNode.value < parentNode.value) {
+                      parentNode.left = leftmost
+                    } else if (currentNode.value > parentNode.value) {
+                      parentNode.right = leftmost
+                    }
+                  }
+                }
+                return true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+const tree = new BinarySearchTree()
+print(tree)
+tree.insert(9)
+tree.insert(4)
+tree.insert(20)
+tree.insert(1)
+tree.insert(6)
+tree.insert(15)
+tree.insert(170)
+print(tree)
+print(tree.remove(170))
+print(tree)
+
+//        9
+//    4       20
+//1     6   15   170
+
+function traverse(node) {
+  const tree = { value: node.value }
+  tree.left = node.left === null ? null : traverse(node.left)
+  tree.right = node.right === null ? null : traverse(node.right)
+  return tree
+}
